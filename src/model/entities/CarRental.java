57a3d8418261107fafa2000ac21670d34bdd4624
4Miguel2 <delgdevoleper@gmail.com>
2024-01-16 14:53:04 -0300

    Program finished without interface

--- src/model/entities/CarRental.java
+++ src/model/entities/CarRental.java
@@ -13,7 +13,7 @@ public class CarRental {
 
     }
 
-    public CarRental(Date start, Date finish, Vehicle vehicle, Invoice invoice) {
+    public CarRental(Date start, Date finish, Vehicle vehicle) {
         this.start = start;
         this.finish = finish;
         this.vehicle = vehicle;
